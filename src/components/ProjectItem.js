92f4a168a10dcff252b946d4c5064cb7982b2be0
Andrew Kibiwott <kibiandrew@gmail.com>
2023-01-19 04:27:24 +0300

    done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  return (
-    <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
-    </div>
-  );
+    const technologiesEl = technologies.map((tech) => {
+        return <span key = { tech } > { tech } < /span>
+    })
+    return ( <
+        div className = "project-item" >
+        <
+        h3 > { name } < /h3> <
+        p > { about } < /p> <
+        div className = "technologies" > { /* render a <span> for each technology in the technologies array */ } { technologiesEl } <
+        /div> <
+        /div>
+    );
 }
 
 export default ProjectItem;
